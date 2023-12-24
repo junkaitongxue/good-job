94559ae398de180edb90da3ebccde03d6742ef35
junkaitongxue <252647920@qq.com>
2023-12-24 13:57:39 +0800

    【describe】解决了使用cooikie进行登录token获取的问题，重复打开页面不需要进行登录操作

--- good-job-admin-web/src/main.ts
+++ good-job-admin-web/src/main.ts
@@ -10,14 +10,15 @@ import VueCookies from 'vue-cookies'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import { ElMessage } from 'element-plus'
+import { cookies } from './utils/cookies'
 
 // 检查登录状态
 async function checkLogin() {
   // 你可以在这里进行登录验证，例如通过请求后端接口或检查本地存储中的登录状态
   // 这里假设使用 store 中的登录状态进行判断
   const isLoggedIn = store.state.isLoggedIn
-  console.log(store.state.isLoggedIn)
-  if (!isLoggedIn) {
+  const token = cookies.getCookie('XXL_JOB_LOGIN_IDENTITY')
+  if ( token == null) {
     // 如果未登录，重定向到登录页面或执行其他操作
     return false
   } 
@@ -38,7 +39,11 @@ async function initializeApp() {
   checkLogin().then(isLoggedIn => {
     if (isLoggedIn) {
       // 如果已登录，挂载应用到根元素上
-      app.mount('#app')
+      ElMessage({
+        message: '已登录，为你跳转到首页',
+        type: 'success',
+      })
+      router.push('/home')
     } else {
       ElMessage({
         message: '未登录，无法访问应用, 请登录',
