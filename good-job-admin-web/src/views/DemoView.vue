9f94939781b6519b0ea10cd2a08b2aaf3276c44f
junkaitongxue <252647920@qq.com>
2024-04-02 22:13:48 +0800

    【describe】添加打开本地应用的调度能力

---
+++ good-job-admin-web/src/views/DemoView.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+
+import type { MainParameter } from '@/models/mainParameter'
+import { inject, onMounted } from 'vue'
+
+const mainParameter = inject('mainParameter') as MainParameter
+onMounted(() => {
+  mainParameter.theme = 'light'
+  mainParameter.currentSelect = 'demo'
+})
+</script>
+
+<template>
+  <div class="demo">
+    <h1>Demo 功能演示页面</h1>
+    <a href="weixin://">WeiXin</a>
+    <!-- 最好是检测自定义协议是否存在 -->
+    <a href="hello://">hello</a>
+  </div>
+</template>
